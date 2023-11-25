678b85bff793ce666d683ab22ef5c90ba60186f7
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-11-26 00:55:27 +0530

    commit

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const header = ()=>{
+const Header = ()=>{
     return (
     <div className="header">
-        <div className="logo-design">
-            <img src="https://www.codester.com/static/uploads/items/000/017/17176/preview-xl.jpg"/>
+        <div className="logo-container">
+            <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYOi9-KJVKszed17zhOdfxitHm7A5wcOrftg&usqp=CAU"/>
         </div>
-        <div className="nav items">
+        <div className="nav-items">
             <ul>
                 <li>home</li>
                 <li>about us</li>
                 <li>contact us</li>
+                <li>cart</li>
             </ul>
         </div>
     </div>
-    )
+    );
 
-}
-const applayout =() =>{
+};
+const Applayout =() =>{
     return(
-        <div className="app"><header/></div>
+        <div className="app">
+            <Header/>
+        </div>
     )
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(applayout);
+root.render(<Applayout/>);
