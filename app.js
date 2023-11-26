fbfb38957e4de406261e9019992df040a1c9512c
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-11-26 15:33:24 +0530

    commit

--- app.js
+++ app.js
@@ -22,12 +22,13 @@ const Header = ()=>{
 };
 const RestaurantCard= (props)=>{
     console.log(props);
-    const{resData}= props
+    const{resData}= props;
     return(
         <div className="res-card" style={{backgroundColor:"#f0f0f0"}}>
             <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ngrkti6g5uh6adiwr37o"/>
             <h4>{resData.restaurants[0].info.avgRating}</h4>
             <h4>{resData.restaurants[0].info.name}</h4>
+            <h4></h4>
             <h4>37 min</h4>
         </div>
     )
