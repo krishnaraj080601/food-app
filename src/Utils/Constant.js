3ce21f4f409c6e52150120ea29d834bed5705ae7
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-11-27 22:35:36 +0530

    commit

---
+++ src/Utils/Constant.js
@@ -0,0 +1,2 @@
+ const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/";
+const LOGO_URL ="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=";
