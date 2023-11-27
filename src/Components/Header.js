1f853e7ce21a1bbdd3b3ba74212c03bdd1569d51
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-11-27 21:55:40 +0530

    commit

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+const Header = ()=>{
+    return (
+    <div className="header">
+        <div className="logo-container">
+            <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="/>
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>home</li>
+                <li>about us</li>
+                <li>contact us</li>
+                <li>cart</li>
+                <li>hello </li>
+            </ul>
+        </div>
+    </div>
+    );
+
+};
--- src/app.js
+++ src/app.js
@@ -1,25 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-const Header = ()=>{
-    return (
-    <div className="header">
-        <div className="logo-container">
-            <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf="/>
-        </div>
-        <div className="nav-items">
-            <ul>
-                <li>home</li>
-                <li>about us</li>
-                <li>contact us</li>
-                <li>cart</li>
-                <li>hello </li>
-            </ul>
-        </div>
-    </div>
-    );
 
-};
 const RestaurantCard= (props)=>{
     //console.log(props);
     const{resData}= props;
