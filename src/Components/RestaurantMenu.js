ee9d4ac672d47a5b5d44eb2914a41545171e84be
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-04 22:33:40 +0530

    commit

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const RestaurantMenu = () => {
+    useEffect=(()=>{},[])
+    const fetchapi1= async ()=>{
+        const data =await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId=50075&catalog_qa=undefined&submitAction=ENTER")
+    const json=await data.json();
+    console.log(json);
+    }
   return (
     <div className='menu'>
         <h1>name of the restaurant</h1>
