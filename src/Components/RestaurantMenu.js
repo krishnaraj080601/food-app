e38b792625ab9d3f8fe3c15eb605989288b2a3d1
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-04 22:45:36 +0530

    commit

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 
 const RestaurantMenu = () => {
-    useEffect=(()=>{fetchMenu()},[])
+    useEffect=(()=>{
+        fetchMenu()
+    },[]);
     const fetchMenu= async ()=>{
-        const data =await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId=50075&catalog_qa=undefined&submitAction=ENTER")
-    const json=await data.json();
+        const data =await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId=50075&catalog_qa=undefined&submitAction=ENTER");
+        const json=await data.json();
     console.log(json);
     }
   return (
