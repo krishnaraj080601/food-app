8a9cae3f9dc5c9160a36e7d163244f05b36817f9
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-04 18:49:16 +0530

    commit

---
+++ src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        <h1>about</h1>
+        <h2>food ordering website</h2>
+    </div>
+  )
+}
+
+export default About
--- src/app.js
+++ src/app.js
@@ -3,13 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
-
-
-
-        
-    
-    
-
+import { createBrowserRouter } from "react-router-dom";
+import About from "./Components/About";
 const Applayout =() =>{
     return(
         <div className="app">
@@ -18,5 +13,17 @@ const Applayout =() =>{
         </div>
     )
 }
+const approuter=createBrowserRouter([
+    {
+        path:"/",
+        element:<Applayout/>
+
+},
+{
+    path:"/About",
+    element:<About/>
+
+}
+])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Applayout/>);
