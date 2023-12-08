fbf26fdfb3d8a6f0f118113a70d9e99b3520c22c
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-08 20:33:06 +0530

    commit

--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -5,12 +5,11 @@ import { MENU_API } from '../Utils/Constant';
 
 const RestaurantMenu = () => {
     const [resInfo,setresinfo]= useState(null)
-    const {resId}=useParams()
     useEffect(() => {
         fetchMenu();
       }, []);
     const fetchMenu=async ()=>{
-        const  data = await fetch(MENU_API+{resId})
+        const  data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId=75217&catalog_qa=undefined&submitAction=ENTER")
      const json = await data.json();
      console.log(json);
      setresinfo(json.data);
@@ -27,10 +26,12 @@ const RestaurantMenu = () => {
   return (
     <div className='menu'>
         <h1>{name}</h1>
-        <p>{cuisines.join(",")}-{costForTwoMessage}</p>
+        <p>{cuisines}</p>
+        <h3>{costForTwoMessage}</h3>
         <h2>menu</h2>
         <ul>
-            {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name}</li>)}
+            {itemCards.map(item=><li key={item.card.info.id}>{item.card.info.name}-{"RS."}
+            {item.card.info.price /100 || item.card.info.defaultprice /100}</li>)}
             </ul>
         </div>
   )
