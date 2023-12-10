0f5dff58fed4e9940b7791147957b6fc91d569e8
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-10 22:25:37 +0530

    commit

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -6,7 +6,7 @@ const RestaurantCard= (props)=>{
     const{name,cloudinaryImageId,cuisines,avgRating}=resData?.info;
     return(
         <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
-            <img className="res-logo" alt="res-logo" src={CDN_URL+cloudinaryImageId}/>
+            <img className="rounded-lg" alt="res-logo" src={CDN_URL+cloudinaryImageId}/>
             <h4 className="font-bold py-4 text-lg" >{name}</h4>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating}</h4>
