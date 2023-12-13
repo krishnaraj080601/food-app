a510a51122ab16970801a65ac0ba6546ccba13b8
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-13 23:10:18 +0530

    commit

--- src/Components/List.js
+++ src/Components/List.js
@@ -7,7 +7,7 @@ const List = ({items}) => {
     <div>
         {items.map((item) =>(
             <div key={item.card.info.id} className='p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between'>
-              
+                  
                 <div className='w-9/12'>
                     <div className='py-2'>
                 <span>{item.card.info.name}</span>
@@ -16,9 +16,20 @@ const List = ({items}) => {
                   ? item.card.info.price / 100
                   : item.card.info.defaultPrice / 100}</span>
                 </div>
+                <p className='tex-3xs'>{item.card.info.description}</p>
                  </div>
-                 <p>{item.card.info.description}</p>
+                 <div className='w-3/12 p-4'>
+                    <div className='absolute'>
+                    <button
+                className="mx-12 p-1 bg-black-700 text-white shadow-lg rounded-lg hover:bg-white hover:text-white-800 font-bold border border-black-500"
+              >
+                Add +
+              </button>
+                        </div>
+                 <img src={CDN_URL + item.card.info.imageId} className="w-full" />
                  </div>
+                 </div>
+                 
         ))}
     </div>
   )
