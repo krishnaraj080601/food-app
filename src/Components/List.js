5674bc0284f6f596d58a3eceebb954df8bce2bb9
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-13 22:40:41 +0530

    commit

--- src/Components/List.js
+++ src/Components/List.js
@@ -9,7 +9,10 @@ const List = ({items}) => {
                 <div className='w-9/12'>
                     <div className='py-2'>
                 <span>{item.card.info.name}</span>
-                <span>{item.card.info.price}</span>
+                <span> - ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}</span>
                 </div>
                  </div>
                  <p>{item.card.info.description}</p>
