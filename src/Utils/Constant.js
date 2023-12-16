43ac464fc606624d25172b21d26b35593391d866
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-16 23:42:49 +0530

    commit

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -1,3 +1,3 @@
  export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/";
 export const LOGO_URL ="https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg";
-export const MENU_API ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId="
+export const MENU_API ="https://kind-puce-bull-tie.cyclic.app/api/proxy/swiggy/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId="
