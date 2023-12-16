83bd9c1174fbe7a42e7fc44ab9f6b823f548fbfc
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-16 13:00:18 +0530

    commit

--- src/Utils/Constant.js
+++ src/Utils/Constant.js
@@ -1,3 +1,3 @@
  export const CDN_URL ="https://media-assets.swiggy.com/swiggy/image/upload/";
-export const LOGO_URL ="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=food&sf=";
+export const LOGO_URL ="https://marketplace.canva.com/EAFpeiTrl4c/1/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-9Gfim1S8fHg.jpg";
 export const MENU_API ="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.0168445&lng=76.9558321&restaurantId="
---
+++ src/Utils/appStore.js
@@ -0,0 +1,4 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+const appStore=configureStore();
+export default appStore;
