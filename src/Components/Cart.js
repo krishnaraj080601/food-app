cf34833623c8a94607019882697f85932f053cac
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-16 15:13:13 +0530

    commit

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart } from '../Utils/cartSlice';
+import List from './List';
 
 const Cart = () => {
+    const cartItems = useSelector((store) => store.cart.items);
+    const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
-    <div>Cart</div>
+    <div className="text-center m-4 p-4">
+    <h1 className="text-2xl font-bold">Cart</h1>
+    <div className="w-6/12 m-auto">
+        <button
+          className=" p-2 m-2 bg-black text-white rounded-lg"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+        <List items={cartItems} />
+        </div>
+    </div>
   )
 }
 
