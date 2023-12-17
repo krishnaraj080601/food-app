a207464045cd2d203356d3aa58ada51fc873887e
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-18 01:09:14 +0530

    commit

--- src/Components/__tests__/search.test.js
+++ src/Components/__tests__/search.test.js
@@ -3,7 +3,7 @@ import Body from "../Body";
 import "@testing-library/jest-dom";
 import MOCK_DATA from "../mocks/mockResListData.json";
 import { act } from "react-dom/test-utils";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 global.fetch = jest.fn(() => {
     return Promise.resolve({
       json: () => {
@@ -21,7 +21,8 @@ it("Should Search Res List for burger text input ", async () => {
     )
 }
     );
-    const searchBtn = screen.getByRole("button", { name: "Search" });
+    const searchBtn = screen.getAllByRole("button", { name: "Search" });
     const searchInput = screen.getByTestId("searchInput");
     fireEvent.change(searchInput, { target: { value: "burger" } });
     fireEvent.click(searchBtn);
+screen.getAllByTestId("rescard")
