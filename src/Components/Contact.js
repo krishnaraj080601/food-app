27fef3eedc5c684cbbcc938c5852855bf1122c38
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-17 18:55:09 +0530

    commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='text-3xl font-bold '>Contact</div>
+    <div>
+      <h1 className="font-bold text-3xl p-4 m-4">Contact us pages</h1>
+      <form>
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className=" border border-black p-2 m-2"
+          placeholder="message"
+        />
+        <button className=" border border-black p-2 m-2 bg-gray-100 rounded-lg">
+          Submit
+        </button>
+      </form>
+      </div>
   )
 }
 
