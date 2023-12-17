ec71fb6666b9b44d9a8adbb62b04a5aa172b0245
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-17 19:17:17 +0530

    commit

--- src/Components/__tests__/Contact.test.js
+++ src/Components/__tests__/Contact.test.js
@@ -5,5 +5,34 @@ import "@testing-library/jest-dom"
 test("should load contactcomponent ",()=>{
     render(<Contact/>)
     const heading = screen.getByRole("heading");
-    expect(heading)
+    expect(heading).toBeInTheDocument();
 } )
+test("Should load button inside Contact component", () => {
+    render(<Contact />);
+
+    const button = screen.getByRole("button");
+
+    // Assertion
+    expect(button).toBeInTheDocument();
+  });
+  it("Should load input name inside Contact component", () => {
+    render(<Contact />);
+
+    const inputName = screen.getByPlaceholderText("name");
+
+    // Assertion
+    expect(inputName).toBeInTheDocument();
+  });
+  it("Should load 2 input boxes on the Contact component", () => {
+    render(<Contact />);
+
+    // Querying
+    const inputBoxes = screen.getAllByRole("textbox");
+
+    //console.log(inputBoxes.length);
+
+    // Assertion
+
+    expect(inputBoxes.length).toBe(2);
+  });
+
