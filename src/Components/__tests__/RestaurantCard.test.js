fab73c4840f9380070bc1bd01f226d95a7826d2e
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-17 23:50:35 +0530

    commit

--- src/Components/__tests__/RestaurantCard.test.js
+++ src/Components/__tests__/RestaurantCard.test.js
@@ -4,6 +4,6 @@ import RestaurantCard from "../RestaurantCard";
 import MOCK_DATA from "../mocks/resCardMock.json";
 it("should render RestaurantCard component with props Data", () => {
     render(<RestaurantCard resdata={MOCK_DATA}/>)
-    const name = screen.getByText("Burma Bhai");
+    const name = screen.getByText("Sri Raja Biriyani Hotel");
     expect(name).toBeInTheDocument();
 })
