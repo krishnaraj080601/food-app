f59d036ac178fad7b0f7e614c8c453e257212229
krishnaraj080601 <krishnarajkrish.01@gmail.com>
2023-12-18 12:26:03 +0530

    commit

--- src/Components/List.js
+++ src/Components/List.js
@@ -14,7 +14,7 @@ const List = ({items}) => {
   return (
     <div>
         {items.map((item) =>(
-            <div key={item.card.info.id} className='p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between'>
+            <div data-testid="foodItems" key={item.card.info.id} className='p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between'>
                   
                 <div className='w-9/12'>
                     <div className='py-2'>
--- src/Components/__tests__/Card.test.js
+++ src/Components/__tests__/Card.test.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import { act } from "react-dom/test-utils"
 import { BrowserRouter } from "react-router-dom"
 import Header from "../Header"
@@ -7,6 +7,7 @@ import Cart from "../Cart";
 import { Provider } from "react-redux"
 import appStore from "../../utils/appStore";
 import "@testing-library/jest-dom"
+import MOCK_DATA_NAME from "../mocks/mockResMenu.json"
 global.fetch = jest.fn(() =>
   Promise.resolve({
     json: () => Promise.resolve(MOCK_DATA_NAME),
@@ -26,4 +27,7 @@ it("should Load Restaurant Menu Component", async () => {
         
     )
     )
+    const accordionHeader = screen.getByText("Recommended (20 )");
+    fireEvent.click(accordionHeader);
+    expect(screen.getAllByTestId("foodItems").length).toBe(20);
 })
